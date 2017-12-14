6c8d2ff484e806e28c5f0a87d6720bfb28316b38
Aitor4 <aitor4p@gmail.com>
2017-12-14 16:40:58 +0100

    Added Adagrad Gradient descent class and modified slighlty AdagradUpdater accordingly

--- mllib/src/main/scala/org/apache/spark/mllib/optimization/AdagradUpdater.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/optimization/AdagradUpdater.scala
@@ -8,28 +8,27 @@ import breeze.numerics.{sqrt => brzSqrt}
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.mllib.linalg.{Matrix, Vector, Vectors}
 
-//TODO: Create Gradient descent class for Adagrad
-//TODO: Check that matrix multiplication size is perormed correctly
+//TODO: Check that matrix multiplication size is performed correctly
 //TODO: Test
-class AdagradUpdaterNaive {
+class AdagradUpdater{
 
   def compute(
                         weightsOld: Vector,
                         gradient: Vector,
                         stepSize: Double,
-                        momentumOld: Vector,
-                        momentumFraction: Double,
                         squareGradients : Vector,
-                        smoothingTerm: Double): (Vector, Double) = {
+                        smoothingTerm: Double,
+                        iter: Int,
+                        regParam : Double): (Vector, Double) = {
     val brzSquared: BV[Double] = squareGradients.asBreeze.toDenseVector
     val brzWeights: BV[Double] = weightsOld.asBreeze.toDenseVector
     val brzGradient: BV[Double] = gradient.asBreeze.toDenseVector
 
     val root = brzSqrt(brzSquared + smoothingTerm)
     val matrix = diag(root)
-    val smoothed = brzGradient *:* stepSize
-    val step =  smoothed * inv(matrix)
-    val weightsNew = brzWeights - step
+    val grad = brzGradient *:* stepSize
+    val update =  grad * inv(matrix)
+    val weightsNew = brzWeights - update
 
     (Vectors.fromBreeze(weightsNew), 0)
   }
